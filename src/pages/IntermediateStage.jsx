2475e3e5d4751b8e4841d66fa49afa3089cd6185
CarolCheng <yuntingcheng@gmail.com>
2024-09-13 16:43:30 +0800

    feat(button): switch between two SVG files for different modes

--- src/pages/IntermediateStage.jsx
+++ src/pages/IntermediateStage.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { log, logLevel } from '@/utils/log';
+import { useTheme } from '@/utils/ThemeContext';
 import Button from '@/components/buttons/Button';
 
 const IntermediateStage = () => {
 
     const { t, i18n } = useTranslation();
-    const svgUrl = require('@/assets/buttons/dot.svg');
+    const { isDarkMode } = useTheme();
+
+    const svgUrl = isDarkMode ? require('@/assets/buttons/dot_dark.svg') : require('@/assets/buttons/dot_light.svg');
 
     log(logLevel.DEBUG, 'IntermediateStage rendered');
 
