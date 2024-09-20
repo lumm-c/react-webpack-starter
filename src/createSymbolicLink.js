e2933835fc07be8fb4de7cfe62bcf0bed9d1f001
CarolCheng <yuntingcheng@gmail.com>
2024-09-20 17:55:56 +0800

    fix symbolic link in vercel environment

--- src/createSymbolicLink.js
+++ src/createSymbolicLink.js
@@ -8,11 +8,14 @@ const directories = [
     { source: path.join(__dirname, '../submodules/open-huninn-font/font'), dest: path.join(__dirname, '/assets/fonts/open-huninn-font') },
 ];
 
-// 刪除現有目錄的函數
-function deleteDirectoryIfExists(dir) {
-    if (fs.existsSync(dir)) {
-        console.log(`Deleting existing directory: ${dir}`);
-        fs.rmSync(dir, { recursive: true, force: true });
+// 檢查並刪除現有目錄或符號連結
+function deleteIfExists(dest) {
+    if (fs.existsSync(dest)) {
+        const stat = fs.lstatSync(dest);
+        if (stat.isSymbolicLink() || stat.isDirectory()) {
+            console.log(`Deleting existing link or directory: ${dest}`);
+            fs.rmSync(dest, { recursive: true, force: true });
+        }
     }
 }
 
@@ -41,8 +44,8 @@ function createSymbolicLink(source, dest) {
 
 // 遍歷每對源目錄和目標目錄
 directories.forEach(({ source, dest }) => {
-    // 刪除現有的目標目錄
-    deleteDirectoryIfExists(dest);
+    // 檢查並刪除現有的目標符號連結或目錄
+    deleteIfExists(dest);
 
     // 創建新的符號連結
     createSymbolicLink(source, dest);
